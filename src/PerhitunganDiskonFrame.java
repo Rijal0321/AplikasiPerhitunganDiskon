f38566ec5b3506952de8b6628774500ff55b237b
Muhammad Rijal Aditya <136682708+Rijal0321@users.noreply.github.com>
2024-11-09 12:48:09 +0800

    menambahkan import javax.swing.*;

--- src/PerhitunganDiskonFrame.java
+++ src/PerhitunganDiskonFrame.java
@@ -1,12 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
- */
+import javax.swing.*;
 
-/**
- *
- * @author User
- */
+
 public class PerhitunganDiskonFrame extends javax.swing.JFrame {
 
     /**
